db5701725492196d5a5e031d21a28fb79fa1b701
Thomaz Felipe <thomaz.silva@neppoadm.com.br>
2018-07-20 09:58:28 -0300

    update peer

--- Peer.java
+++ Peer.java
@@ -6,7 +6,7 @@ public class Peer {
     public static void main(String[] args) throws UnknownHostException, IOException {
         new Thread(() -> {
             try {
-                ServerSocket server = new ServerSocket(3322);
+                ServerSocket server = new ServerSocket(3323);
                 System.out.println("Server initialized in port 3322");
 
                 Socket client = server.accept();
@@ -17,20 +17,39 @@ public class Peer {
                 while (input.hasNextLine()) {
                     System.out.println(client.getInetAddress().getHostAddress() + " say: " + input.nextLine());
                 }
+
+                input.close();
+                server.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }).start();
 
-        Scanner reader = new Scanner(System.in);    
+        Socket client;
+        Scanner reader = new Scanner(System.in);
         System.out.print("Type IP's user which you want connect: ");
-        Socket client = new Socket(reader.nextLine(), 3322);
-        System.out.println("The client connected!");
 
-        PrintStream output = new PrintStream(client.getOutputStream());
+        while(true){
+            try {
+                client = new Socket(reader.nextLine(), 3322);
+                System.out.println("The client connected!");
 
-        while (reader.hasNextLine()) {
-            output.println(reader.nextLine());
+                PrintStream output = new PrintStream(client.getOutputStream());
+
+                while (reader.hasNextLine()) {
+                    if (reader.nextLine().equals("/exit")) {
+                        System.out.println("Bye!!!");
+                        reader.close();
+                        client.close();
+                        output.close();
+                        System.exit(1);
+                    }
+                    output.println(reader.nextLine());
+                }
+            } catch (Exception e) {
+                System.out.print("Connection failed! Try again: ");
+                continue;
+            }
         }
     }
 }
